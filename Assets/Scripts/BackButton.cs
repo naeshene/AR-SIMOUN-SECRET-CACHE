6d8609957f7d1c071510f28d609a239a4ed11e67
naeshene <153047577+naeshene@users.noreply.github.com>
2025-09-05 15:47:26 +0800

    Backbutton

--- Assets/Scripts/BackButton.cs
+++ Assets/Scripts/BackButton.cs
@@ -5,10 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class BackButton : MonoBehaviour
 {
-
+   
     public void GoToMainMenu()
     {
         SceneManager.LoadScene("MainMenu"); // Go to Main Menu
+    }
+
+    public void GoToLevels()
+    {
+        SceneManager.LoadScene("MainMenu"); // Go to Levels
+    }
+
+    public void GoToAlbum()
+    {
+        SceneManager.LoadScene("MainMenu"); // Go to Album
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MainMenu"); // Go to Menu
+
+    public void GoToTutorial()
+    {
+        SceneManager.LoadScene("MainMenu"); // Go to Tutorial
+    }
+
+    public void GoToSettings()
+    {
+        SceneManager.LoadScene("MainMenu"); // Go to Settings
+    }
+
+    public void GoToAbout()
+    {
+        SceneManager.LoadScene("MainMenu"); // Go to About
+    }
+
+    public void GoToExit()
+    {
+        SceneManager.LoadScene("MainMenu"); // Go to Exit
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is quitting..."); // Works only on device, not in editor
     }
 
 }
